daf54c45a53029c92ca896244591de9a4ab2f1bc
Daniel Mössner <github@moesibert.de>
2023-06-29 17:14:47 +0200

    remove the negative margin

--- src/components/TableTable.vue
+++ src/components/TableTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col">
-    <div class="-mx-6 -my-2">
-      <div class="min-w-full px-6 py-2">
+    <div class="-my-2">
+      <div class="min-w-full px-0 py-2">
         <div class="overflow-hidden rounded-md shadow">
           <div class="overflow-auto">
             <table class="min-w-full divide-y divide-gray-200">
