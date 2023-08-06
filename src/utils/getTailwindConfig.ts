48f3fc5d4a02f4cf879a5399c4bf14ee048e4eaa
Daniel Mössner <github@moesibert.de>
2023-08-06 14:04:57 +0200

    fix line stuff

--- src/utils/getTailwindConfig.ts
+++ src/utils/getTailwindConfig.ts
@@ -66,8 +66,6 @@ export default function getTailwindConfig(content: string[]) {
         }),
       },
     },
-    plugins: [
-      require("@tailwindcss/typography"),
-    ],
+    plugins: [require("@tailwindcss/typography")],
   };
 }
