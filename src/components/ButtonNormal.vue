382c675245c2a389476b5be0b27ff77bd39362f2
Daniel Mössner <github@moesibert.de>
2024-07-24 12:10:55 +0200

    add outline style

--- src/components/ButtonNormal.vue
+++ src/components/ButtonNormal.vue
@@ -11,116 +11,109 @@
     </component>
   </div>
 </template>
-
-<script lang="ts">
-import { defineComponent, PropType } from "vue";
+<script lang="ts" setup>
+import { computed, toRefs } from "vue";
 import Loader from "./CircleLoader.vue";
 
-export default defineComponent({
-  components: {
-    Loader,
+const props = withDefaults(
+  defineProps<{
+    to?: string | object | null;
+    id?: string;
+    href?: string;
+    type?: string;
+    loading?: boolean;
+    disabled?: boolean;
+    kind?: "primary" | "secondary" | "action" | "delete" | "link" | "outline";
+  }>(),
+  {
+    to: null,
+    id: undefined,
+    href: undefined,
+    type: "button",
+    loading: false,
+    disabled: false,
+    kind: "primary",
   },
-  props: {
-    to: {
-      type: [String, Object],
-      default: null,
-    },
-    id: {
-      type: String,
-      default: undefined,
-    },
-    href: {
-      type: String,
-      default: null,
-    },
-    type: {
-      type: String,
-      default: "button",
-    },
-    loading: {
-      type: Boolean,
-      default: false,
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-    kind: {
-      type: String as PropType<
-        "primary" | "secondary" | "action" | "delete" | "link"
-      >,
-      default: "primary",
-    },
-  },
-  emits: ["click"],
-  computed: {
-    attrs() {
-      let attrs = {} as { [key: string]: any };  // eslint-disable-line
-      if (this.type && this.is === "button") {
-        attrs["type"] = this.type;
-        attrs["disabled"] = this.disabledComputed;
-      } else if (this.is === "router-link") {
-        attrs["to"] = this.to;
-      } else if (this.is === "a") {
-        attrs["href"] = this.href;
-      }
-      if (this.id) attrs["id"] = this.id;
-      return attrs;
-    },
-    is() {
-      if (this.to) {
-        return "router-link";
-      } else if (this.href) {
-        return "a";
-      } else if (this.type) {
-        return "button";
-      }
-      return "div";
-    },
-    disabledComputed() {
-      if (this.loading || this.disabled) return true;
-      return false;
-    },
-    baseClasses() {
-      return "cursor-pointer inline-flex items-center focus:outline-none";
-    },
-    kindColorClasses() {
-      if (this.kind === "primary") {
-        return "border shadow-sm border-transparent text-white bg-formcolor bg-opacity-100 hover:bg-opacity-90 focus:ring-2 focus:ring-offset-2 focus:ring-formcolor";
-      } else if (this.kind === "secondary") {
-        return "border shadow-sm border-transparent text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:ring-2 focus:ring-offset-2";
-      } else if (this.kind === "action") {
-        return "border border-transparent text-blue-700 hover:text-blue-900 hover:bg-blue-50 focus:border-blue-200";
-      } else if (this.kind === "delete") {
-        return "border border-transparent text-red-600 hover:text-red-700 hover:bg-red-50 focus:border-red-200";
-      } else if (this.kind === "link") {
-        return "underline text-formcolor hover:text-opacity-75 focus:ring-0";
-      }
-      return "";
-    },
-    kindSizeClasses() {
-      if (this.kind === "action" || this.kind === "delete") {
-        return "px-2 py-0.5 text-sm font-medium rounded-md -mx-2 hover:z-10 hover:relative";
-      } else if (this.kind === "link") {
-        return "";
-      } else if (this.kind === "primary" || this.kind === "secondary") {
-        return "px-4 py-2 text-sm font-medium rounded-xl";
-      }
-      return "";
-    },
-    moreClasses() {
-      let moreClasses = "";
+);
+const { to, href, type, id, disabled, loading } = toRefs(props);
 
-      if (this.disabledComputed)
-        moreClasses += "pointer-events-none opacity-80 cursor-not-allowed";
+const emit = defineEmits(["click"]);
 
-      return moreClasses;
-    },
-  },
-  methods: {
-    emitClick() {
-      this.$emit("click");
-    },
-  },
+const is = computed(() => {
+  if (to.value) {
+    return "router-link";
+  } else if (href.value) {
+    return "a";
+  } else if (type.value) {
+    return "button";
+  }
+  return "div";
 });
+
+const attrs = computed(() => {
+  let attrs = {} as { [key: string]: any };  // eslint-disable-line
+  if (type.value && is.value === "button") {
+    attrs["type"] = type.value;
+    attrs["disabled"] = disabledComputed.value;
+  } else if (is.value === "router-link") {
+    attrs["to"] = to.value;
+  } else if (is.value === "a") {
+    attrs["href"] = href.value;
+  }
+  if (id.value) attrs["id"] = id.value;
+  return attrs;
+});
+
+const disabledComputed = computed(() => {
+  if (loading.value || disabled.value) return true;
+  return false;
+});
+
+const baseClasses =
+  "cursor-pointer inline-flex items-center focus:outline-none transition duration-100";
+
+const kindColorClasses = computed(() => {
+  if (props.kind === "primary") {
+    return "border shadow-sm border-transparent text-white bg-formcolor bg-opacity-100 hover:bg-opacity-90 focus:ring-2 focus:ring-offset-2 focus:ring-formcolor";
+  } else if (props.kind === "secondary") {
+    return "border shadow-sm border-transparent text-indigo-700 bg-indigo-100 hover:bg-indigo-200 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-200";
+  } else if (props.kind === "outline") {
+    return "border shadow-sm border-indigo-600 text-indigo-500 bg-transparent hover:bg-indigo-500 hover:text-indigo-50 focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500";
+  } else if (props.kind === "action") {
+    return "border border-transparent text-blue-700 hover:text-blue-900 hover:bg-blue-50 focus:border-blue-200";
+  } else if (props.kind === "delete") {
+    return "border border-transparent text-red-600 hover:text-red-700 hover:bg-red-50 focus:border-red-200";
+  } else if (props.kind === "link") {
+    return "underline text-formcolor hover:text-opacity-75 focus:ring-0";
+  }
+  return "";
+});
+
+const kindSizeClasses = computed(() => {
+  if (props.kind === "action" || props.kind === "delete") {
+    return "px-2 py-0.5 text-sm font-medium rounded-md -mx-2 hover:z-10 hover:relative";
+  } else if (props.kind === "link") {
+    return "";
+  } else if (
+    props.kind === "primary" ||
+    props.kind === "secondary" ||
+    props.kind === "outline"
+  ) {
+    return "px-4 py-2 text-sm font-medium rounded-xl";
+  }
+  return "";
+});
+
+const moreClasses = computed(() => {
+  let moreClasses = "";
+
+  if (disabledComputed.value)
+    moreClasses += "pointer-events-none opacity-80 cursor-not-allowed";
+
+  return moreClasses;
+});
+
+const emitClick = () => {
+  emit("click");
+};
 </script>
---
+++ src/views/ButtonTest.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import ButtonNormal from "@/components/ButtonNormal.vue";
+</script>
+
+<template>
+  <div class="p-10">
+    <div class="flex flex-col gap-4 p-10 border">
+      <ButtonNormal kind="primary" @click="null">
+        Reorder Properties
+      </ButtonNormal>
+      <ButtonNormal kind="secondary" @click="null">
+        Reorder Properties
+      </ButtonNormal>
+      <ButtonNormal kind="outline" @click="null">
+        Reorder Properties
+      </ButtonNormal>
+      <ButtonNormal kind="action" @click="null">
+        Reorder Properties
+      </ButtonNormal>
+      <ButtonNormal kind="delete" @click="null">
+        Reorder Properties
+      </ButtonNormal>
+      <ButtonNormal kind="link" @click="null">Reorder Properties</ButtonNormal>
+    </div>
+  </div>
+</template>
